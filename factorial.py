d40cadd9e690c67d11c1a086038f1eea465b569b
Shaima Anvar <shaima.anvar@gmail.com>
2025-01-15 08:47:40 -0500

    reverse array

---
+++ factorial.py
@@ -0,0 +1,3 @@
+def factorial(n):
+    print(n*(n-1)*(n-2)*(n-3));
+factorial(3);
---
+++ rev_array.py
@@ -0,0 +1,7 @@
+def rev_array(a):
+    a.reverse();
+    return a;
+print(rev_array([1,2,3,4,5]));
+# a=[1,2,3];
+# a.reverse();
+# # print(a);
